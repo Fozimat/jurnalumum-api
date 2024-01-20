0846730d7afdb5b65e28e8dd5f64a23895d3259e
fozimat <fozimata@gmail.com>
2024-01-20 22:15:48 +0700

    add users seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Ozi',
+            'email' => 'ozi@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
