938ef98030fb136f67fec9b26e868cf3ffe65661
fozimat <fozimata@gmail.com>
2024-01-25 01:05:50 +0700

    make helper for responses

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Response;
+
+class ResponseHelper
+{
+    public static function success($data = [], $message = 'Success', $code = Response::HTTP_OK)
+    {
+        return response()->json([
+            'code' => $code,
+            'success' => true,
+            'messages' => $message,
+            'data' => $data,
+        ]);
+    }
+
+    public static function error($message = 'Error', $code = Response::HTTP_BAD_REQUEST)
+    {
+        return response()->json([
+            'code' => $code,
+            'success' => false,
+            'messages' => $message,
+        ]);
+    }
+}
