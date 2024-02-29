93b33cff58b6e9a79c271f45e541e3026501aeac
fozimat <fozimata@gmail.com>
2024-02-29 22:38:56 +0700

    update (routes api) : add prefix

--- routes/api.php
+++ routes/api.php
@@ -21,30 +21,39 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('/auth/register', [AuthController::class, 'register']);
     Route::post('/auth/login', [AuthController::class, 'login']);
+
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-        Route::get('/kategori', [KategoriController::class, 'index']);
-        Route::get('/kategori/{id}', [KategoriController::class, 'show']);
-        Route::post('/kategori', [KategoriController::class, 'store']);
-        Route::put('/kategori/{id}', [KategoriController::class, 'update']);
-        Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
+        Route::prefix('kategori')->group(function () {
+            Route::get('/', [KategoriController::class, 'index']);
+            Route::get('/{id}', [KategoriController::class, 'show']);
+            Route::post('/', [KategoriController::class, 'store']);
+            Route::put('/{id}', [KategoriController::class, 'update']);
+            Route::delete('/{id}', [KategoriController::class, 'destroy']);
+        });
 
-        Route::get('/sub-kategori', [SubKategoriController::class, 'index']);
-        Route::get('/sub-kategori/{id}', [SubKategoriController::class, 'show']);
-        Route::post('/sub-kategori', [SubKategoriController::class, 'store']);
-        Route::put('/sub-kategori/{id}', [SubKategoriController::class, 'update']);
-        Route::delete('/sub-kategori/{id}', [SubKategoriController::class, 'destroy']);
+        Route::prefix('sub-kategori')->group(function () {
+            Route::get('/', [SubKategoriController::class, 'index']);
+            Route::get('/{id}', [SubKategoriController::class, 'show']);
+            Route::post('/', [SubKategoriController::class, 'store']);
+            Route::put('/{id}', [SubKategoriController::class, 'update']);
+            Route::delete('/{id}', [SubKategoriController::class, 'destroy']);
+        });
 
-        Route::get('/akun', [AkunController::class, 'index']);
-        Route::get('/akun/{id}', [AkunController::class, 'show']);
-        Route::get('/akun/sub-kategori/{id}', [AkunController::class, 'showSubKategori']);
-        Route::post('/akun', [AkunController::class, 'store']);
-        Route::put('/akun/{id}', [AkunController::class, 'update']);
-        Route::delete('/akun/{id}', [AkunController::class, 'destroy']);
+        Route::prefix('akun')->group(function () {
+            Route::get('/', [AkunController::class, 'index']);
+            Route::get('/{id}', [AkunController::class, 'show']);
+            Route::get('/sub-kategori/{id}', [AkunController::class, 'showSubKategori']);
+            Route::post('/', [AkunController::class, 'store']);
+            Route::put('/{id}', [AkunController::class, 'update']);
+            Route::delete('/{id}', [AkunController::class, 'destroy']);
+        });
 
-        Route::get('/jurnal', [JurnalController::class, 'index']);
-        Route::get('/jurnal/{id}', [JurnalController::class, 'show']);
-        Route::post('/jurnal', [JurnalController::class, 'store']);
+        Route::prefix('jurnal')->group(function () {
+            Route::get('/', [JurnalController::class, 'index']);
+            Route::get('/{id}', [JurnalController::class, 'show']);
+            Route::post('/', [JurnalController::class, 'store']);
+        });
     });
 });
